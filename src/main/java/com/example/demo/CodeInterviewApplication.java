8ab49e13d9017631de141569f2ea8d1b80e82c22
Ken Dong <yid164@mail.usask.ca>
2020-04-09 18:43:15 -0600

    Initialize SpringBoot

---
+++ src/main/java/com/example/demo/CodeInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInterviewApplication.class, args);
+    }
+
+}
