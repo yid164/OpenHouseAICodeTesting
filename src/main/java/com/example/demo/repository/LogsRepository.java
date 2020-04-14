4fed42c4756a48cf3926bf4ecb97f06ba33f724b
Ken Dong <yid164@mail.usask.ca>
2020-04-14 11:48:45 -0600

    Added searching for doing types and userId

--- src/main/java/com/example/demo/repository/LogsRepository.java
+++ src/main/java/com/example/demo/repository/LogsRepository.java
@@ -3,12 +3,16 @@ package com.example.demo.repository;
 import com.example.demo.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+import static org.springframework.data.mongodb.repository.Aggregation.*;
 
 @Repository
 public class LogsRepository {
@@ -46,19 +50,19 @@ public class LogsRepository {
     }
 
     /**
-     *
+     * Find UserLog by UserId and Time
      * @param userId
      * @param time
      * @return
      */
     public List<User> findUserLogByUserIdAndTime(String userId, String time){
         Query query = new Query();
-        query.addCriteria(Criteria.where("userId").is(userId).andOperator(Criteria.where("time").is(time)));
+        query.addCriteria(Criteria.where("userId")).fields().elemMatch("actions", Criteria.where("time").is(time));
         return mongoTemplate.find(query, User.class);
     }
 
     /**
-     *
+     * Find User Log by Time
      * @param time
      * @return
      */
@@ -68,6 +72,45 @@ public class LogsRepository {
         return mongoTemplate.find(query,User.class);
     }
 
+    /**
+     * Find all typed users
+     * @return users
+     */
+    public List<User> findAllTypedUsers(){
+        Criteria criteria = Criteria.where("actions.type").ne(null).and("userId").ne(null);
+        TypedAggregation<User> aggregation = newAggregation(User.class,
+                match(criteria));
+        AggregationResults<User> results = mongoTemplate.aggregate(aggregation, User.class);
+        return results.getMappedResults();
+
+    }
+
+    /**
+     * Find typed user in a specific type
+     * @param type type
+     * @return users
+     */
+    public List<User> findUserLogByType(String type){
+        Criteria criteria = Criteria.where("actions.type").is(type).and("userId").ne(null);
+        TypedAggregation<User> aggregation = newAggregation(User.class,
+                match(criteria));
+        AggregationResults<User> results = mongoTemplate.aggregate(aggregation, User.class);
+        return results.getMappedResults();
+    }
+
+    /**
+     * Find typed users in a specific type and userId
+     * @param userId userId
+     * @param type  type
+     * @return users
+     */
+    public List<User> findUserLogByIdandType(String userId, String type){
+        Criteria criteria = Criteria.where("actions.type").is(type).and("userId").is(userId);
+        TypedAggregation<User> aggregation = newAggregation(User.class,
+                match(criteria));
+        AggregationResults<User> results = mongoTemplate.aggregate(aggregation, User.class);
+        return results.getMappedResults();
+    }
 
     /**
      * Delete a user by userId
--- src/main/java/com/example/demo/restful/UserRestController.java
+++ src/main/java/com/example/demo/restful/UserRestController.java
@@ -2,14 +2,12 @@ package com.example.demo.restful;
 
 import com.example.demo.entities.User;
 import com.example.demo.repository.LogsRepository;
-import com.example.demo.repository.UserRepository;
 import com.oracle.tools.packager.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Restful controller for doing the POST, GET, and Delete
@@ -92,6 +90,39 @@ public class UserRestController {
     public List<User> getUsersByTime(@PathVariable String time){
         Log.info("GET user by time");
         return logsRepository.findUserLogByTime(time);
+    }
+
+    /**
+     * Get all typed users
+     * @return typed users
+     */
+    @RequestMapping(value = "/type", method = RequestMethod.GET)
+    public List<User> getsAllTypeUsers(){
+        Log.info("GET all typed users");
+        return logsRepository.findAllTypedUsers();
+    }
+
+    /**
+     * Get all users for a specific type
+     * @param type the specific type
+     * @return users
+     */
+    @RequestMapping(value = "/type/{type}", method = RequestMethod.GET)
+    public List<User> getsTypeUsers(@PathVariable String type){
+        Log.info("GET the typed users");
+        return logsRepository.findUserLogByType(type);
+    }
+
+    /**
+     * Get all users for a specific types and userId
+     * @param type String type
+     * @param userId String userId
+     * @return users
+     */
+    @RequestMapping(value = "/type/{type}/{userId}", method = RequestMethod.GET)
+    public List<User> getsTypeIdUsers(@PathVariable String type, @PathVariable String userId){
+        Log.info("GET the typed users");
+        return logsRepository.findUserLogByIdandType(userId, type);
     }
 
 }
